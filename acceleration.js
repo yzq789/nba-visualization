e15fc05d3543f70db72b641b7965fdd7641a1713
mansteinliliang_i <mansteinliliang_i@didichuxing.com>
2018-06-22 15:24:48 +0800

    李亮部分完成

--- acceleration.js
+++ acceleration.js
@@ -4,283 +4,290 @@ var court_top_num = 570;
 var court_left_num = 25;
 var court_acc_left_num = 25 + 740;
 
-var	court_width = "658px";
-var	court_height =  "385px";
-var	court_left = court_left_num + "px";
-var	court_top = court_top_num + "px";
+var court_width = "658px";
+var court_height = "385px";
+var court_left = court_left_num + "px";
+var court_top = court_top_num + "px";
 var court_acc_left = court_acc_left_num + "px";
 
 
-function updateSpeedAndAcceleration(playerId){
+function updateSpeedAndAcceleration(playerId) {
 
-	if(!firstPainted){
-		firstPainted = true;
-		return;
-	}
-	console.log("updateSpeedAndAcceleration: playerId=" + playerId);
-	updateSpeed(playerId);
-	updateAcceleration(playerId);
+    if (!firstPainted) {
+        firstPainted = true;
+        return;
+    }
+    console.log("updateSpeedAndAcceleration: playerId=" + playerId);
+    updateSpeed(playerId);
+    updateAcceleration(playerId);
 
 }
 
-function updateAcceleration(playerId){
+function updateAcceleration(playerId) {
 
-	d3.select("#main_panel").append("div").attr("id", "acc-wrap-div").style("position", "relative");
-
-
-	d3.select("#acc-wrap-div").append("img")
-		.attr("id", "acc-court-img")
-		.attr("src", "./court.png")
-		.style("width", court_width)
-		.style("height", court_height)
-		.style("opacity", "0.5");
-
-	d3.select("#acc-wrap-div").append("div")
-		.style("position", "absolute")
-		.style("width", court_width)
-		.style("height", court_height)
-		.style("left", 0)
-		.style("top", 0)
-		.style("opacity", "1")
-		.style("background", "#E3E6E6")
-		.style("z-index", "-100");
+    d3.select("#main_panel").append("div").attr("id", "acc-wrap-div").style("position", "relative");
 
 
-	d3.csv("./acc-data/player-" + playerId + "-cell.csv", function(error, data) {
+    d3.select("#acc-wrap-div").append("img")
+        .attr("id", "acc-court-img")
+        .attr("src", "./court.png")
+        .style("width", court_width)
+        .style("height", court_height)
+        .style("opacity", "0.5");
 
-		var cell_size = 14;
+    d3.select("#acc-wrap-div").append("div")
+        .style("position", "absolute")
+        .style("width", court_width)
+        .style("height", court_height)
+        .style("left", 0)
+        .style("top", 0)
+        .style("opacity", "1")
+        .style("background", "#E3E6E6")
+        .style("z-index", "-100");
 
-		var svg = d3.select("#acc-wrap-div").append("svg")
-			.attr("class", "acc-svg")
-		    .style("position", "absolute")
-		    .style("width", court_width)
-		    .style("height", court_height)
-		    .style("left", 0)
-		    .style("top", 0);
 
-		cells = svg.selectAll(".cell")
-			.data(data)
-			.enter()
-			.append("g")
-			.attr("transform", function(d, i){ return "translate( " + i % 47 * cell_size+ ", " + parseInt(i / 47) * cell_size + ")";})
-			.attr("class", "cell")
-			.attr("width", cell_size)
-			.attr("height", cell_size);
+    d3.csv("./acc-data/player-" + playerId + "-cell.csv", function (error, data) {
 
-		cells.each(function(d, i){
+        var cell_size = 14;
 
-			var cell = d3.select(this);
+        var svg = d3.select("#acc-wrap-div").append("svg")
+            .attr("class", "acc-svg")
+            .style("position", "absolute")
+            .style("width", court_width)
+            .style("height", court_height)
+            .style("left", 0)
+            .style("top", 0);
 
-			cell.append("line")
-				.attr("x1", cell_size / 2 - (d["acc_x"]) * cell_size * 5)
-				.attr("y1", cell_size / 2 - (d["acc_y"]) * cell_size * 5)
-				.attr("x2", cell_size / 2)
-				.attr("y2", cell_size / 2)
-				.attr("transform", "rotate(15, " + cell_size / 2 + " " + cell_size/2 + ")")
-				.style("stroke", acc_color)
-				.style("stroke-width", acc_bold)
-				.on("mouseover", function(d){
-					acc_mouseover(d, cell, cell_size);
-            	})
-	            .on("mouseout", function(){
-	                cell.select(".rect_layer").remove();
-	                cell.select(".text_layer").remove();
-	            });
+        cells = svg.selectAll(".cell")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("transform", function (d, i) {
+                return "translate( " + i % 47 * cell_size + ", " + parseInt(i / 47) * cell_size + ")";
+            })
+            .attr("class", "cell")
+            .attr("width", cell_size)
+            .attr("height", cell_size);
 
-			cell.append("line")
-				.attr("x1", cell_size / 2 - (d["acc_x"]) * cell_size * 5)
-				.attr("y1", cell_size / 2 - (d["acc_y"]) * cell_size * 5)
-				.attr("x2", cell_size / 2)
-				.attr("y2", cell_size / 2)
-				.attr("transform", "rotate(-15, " + cell_size / 2 + " " + cell_size/2 + ")")
-				.style("stroke", acc_color)
-				.style("stroke-width", acc_bold)
-				.on("mouseover", function(d){
-					acc_mouseover(d, cell, cell_size);
-            	})
-	            .on("mouseout", function(){
-	                cell.select(".rect_layer").remove();
-	                cell.select(".text_layer").remove();
-	            });
-		})
-	})
+        cells.each(function (d, i) {
+
+            var cell = d3.select(this);
+
+            cell.append("line")
+                .attr("x1", cell_size / 2 - (d["acc_x"]) * cell_size * 5)
+                .attr("y1", cell_size / 2 - (d["acc_y"]) * cell_size * 5)
+                .attr("x2", cell_size / 2)
+                .attr("y2", cell_size / 2)
+                .attr("transform", "rotate(15, " + cell_size / 2 + " " + cell_size / 2 + ")")
+                .style("stroke", acc_color)
+                .style("stroke-width", acc_bold)
+                .on("mouseover", function (d) {
+                    acc_mouseover(d, cell, cell_size);
+                })
+                .on("mouseout", function () {
+                    cell.select(".rect_layer").remove();
+                    cell.select(".text_layer").remove();
+                });
+
+            cell.append("line")
+                .attr("x1", cell_size / 2 - (d["acc_x"]) * cell_size * 5)
+                .attr("y1", cell_size / 2 - (d["acc_y"]) * cell_size * 5)
+                .attr("x2", cell_size / 2)
+                .attr("y2", cell_size / 2)
+                .attr("transform", "rotate(-15, " + cell_size / 2 + " " + cell_size / 2 + ")")
+                .style("stroke", acc_color)
+                .style("stroke-width", acc_bold)
+                .on("mouseover", function (d) {
+                    acc_mouseover(d, cell, cell_size);
+                })
+                .on("mouseout", function () {
+                    cell.select(".rect_layer").remove();
+                    cell.select(".text_layer").remove();
+                });
+        })
+    })
 }
 
 
-function updateSpeed(playerId){
+function updateSpeed(playerId) {
 
-d3.select("#main_panel").append("div").attr("id", "speed-wrap-div").style("position", "relative");
+    d3.select("#main_panel").append("div").attr("id", "speed-wrap-div").style("position", "relative");
 
-d3.select("#speed-wrap-div").append("img")
-	.attr("id", "speed-court-img")
-	.attr("src", "./court.png")
-	.style("width", court_width)
-	.style("height", court_height)
-	.style("opacity", "0.5");
+    d3.select("#speed-wrap-div").append("img")
+        .attr("id", "speed-court-img")
+        .attr("src", "./court.png")
+        .style("width", court_width)
+        .style("height", court_height)
+        .style("opacity", "0.5");
 
-d3.select("#speed-wrap-div").append("div")
-	.style("position", "absolute")
-	.style("width", court_width)
-	.style("height", court_height)
-	.style("left", 0)
-	.style("top", 0)
-	.style("opacity", "1")
-	.style("background", "#E3E6E6")
-	.style("z-index", "-100");
-
-
-	d3.csv("./speed-data/player-" + playerId + "-cell.csv", function(error, data) {
-
-		var cell_size = 56;
-
-		domain = {}
-
-		domain["speed_x"] = d3.extent(data, function(d) { return d["speed_x"]; });
-		domain["speed_y"] = d3.extent(data, function(d) { return d["speed_y"]; });
-
-		var svg = d3.select("#speed-wrap-div").append("svg")
-			.attr("class", "speed-svg")
-		    .style("position", "absolute")
-		    .style("width", court_width)
-		    .style("height", court_height)
-		    .style("left", 0)
-		    .style("top", 0);
-
-		cells = svg.selectAll(".cell")
-			.data(data)
-			.enter()
-			.append("g")
-			.attr("transform", function(d, i){ return "translate( " + i % 12 * cell_size+ ", " + parseInt(i / 12) * cell_size + ")";})
-			.attr("class", "cell")
-			.attr("width", cell_size)
-			.attr("height", cell_size);
-
-		var scale_x =  d3.scale.linear()
-				.domain(domain["speed_x"])
-    			.range([domain["speed_x"][0] * 0.5, domain["speed_x"][1]]);
-
-		var scale_y =  d3.scale.linear()
-				.domain(domain["speed_y"])
-    			.range([domain["speed_y"][0] * 0.5, domain["speed_y"][1]]);
+    d3.select("#speed-wrap-div").append("div")
+        .style("position", "absolute")
+        .style("width", court_width)
+        .style("height", court_height)
+        .style("left", 0)
+        .style("top", 0)
+        .style("opacity", "1")
+        .style("background", "#E3E6E6")
+        .style("z-index", "-100");
 
 
-		cells.each(function(d, i){
+    d3.csv("./speed-data/player-" + playerId + "-cell.csv", function (error, data) {
 
-			var cell = d3.select(this);
+        var cell_size = 56;
+
+        domain = {}
+
+        domain["speed_x"] = d3.extent(data, function (d) {
+            return d["speed_x"];
+        });
+        domain["speed_y"] = d3.extent(data, function (d) {
+            return d["speed_y"];
+        });
+
+        var svg = d3.select("#speed-wrap-div").append("svg")
+            .attr("class", "speed-svg")
+            .style("position", "absolute")
+            .style("width", court_width)
+            .style("height", court_height)
+            .style("left", 0)
+            .style("top", 0);
+
+        cells = svg.selectAll(".cell")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("transform", function (d, i) {
+                return "translate( " + i % 12 * cell_size + ", " + parseInt(i / 12) * cell_size + ")";
+            })
+            .attr("class", "cell")
+            .attr("width", cell_size)
+            .attr("height", cell_size);
+
+        var scale_x = d3.scale.linear()
+            .domain(domain["speed_x"])
+            .range([domain["speed_x"][0] * 0.5, domain["speed_x"][1]]);
+
+        var scale_y = d3.scale.linear()
+            .domain(domain["speed_y"])
+            .range([domain["speed_y"][0] * 0.5, domain["speed_y"][1]]);
 
 
-			cell.append("line")
-				.attr("x1", cell_size / 2 - scale_x(d["speed_x"]) * cell_size )
-				.attr("y1", cell_size / 2 - scale_y(d["speed_y"]) * cell_size )
-				.attr("x2", cell_size / 2)
-				.attr("y2", cell_size / 2)
-				.attr("transform", "rotate(15, " + cell_size / 2 + " " + cell_size/2 + ")")
-				.style("stroke", speed_color)
-				.style("stroke-width", speed_bold)
-				.on("mouseover", function(d){
-					speed_mouseover(d, cell);
-            	})
-	            .on("mouseout", function(){
-	                cell.select(".rect_layer").remove();
-	                cell.select(".text_layer").remove();
-	            })
+        cells.each(function (d, i) {
 
-			cell.append("line")
-				.attr("x1", cell_size / 2 - scale_x(d["speed_x"]) * cell_size * 1.2)
-				.attr("y1", cell_size / 2 - scale_y(d["speed_y"]) * cell_size * 1.2)
-				.attr("x2", cell_size / 2)
-				.attr("y2", cell_size / 2)
-				.attr("transform", "rotate(-15, " + cell_size / 2 + " " + cell_size/2 + ")")
-				.style("stroke", speed_color)
-				.style("stroke-width", speed_bold)
-				.on("mouseover", function(d){
-					speed_mouseover(d, cell);
-            	})
-	            .on("mouseout", function(){
-	                cell.select(".rect_layer").remove();
-	                cell.select(".text_layer").remove();
-	            })
-		})
-	})
+            var cell = d3.select(this);
+
+
+            cell.append("line")
+                .attr("x1", cell_size / 2 - scale_x(d["speed_x"]) * cell_size)
+                .attr("y1", cell_size / 2 - scale_y(d["speed_y"]) * cell_size)
+                .attr("x2", cell_size / 2)
+                .attr("y2", cell_size / 2)
+                .attr("transform", "rotate(15, " + cell_size / 2 + " " + cell_size / 2 + ")")
+                .style("stroke", speed_color)
+                .style("stroke-width", speed_bold)
+                .on("mouseover", function (d) {
+                    speed_mouseover(d, cell);
+                })
+                .on("mouseout", function () {
+                    cell.select(".rect_layer").remove();
+                    cell.select(".text_layer").remove();
+                })
+
+            cell.append("line")
+                .attr("x1", cell_size / 2 - scale_x(d["speed_x"]) * cell_size * 1.2)
+                .attr("y1", cell_size / 2 - scale_y(d["speed_y"]) * cell_size * 1.2)
+                .attr("x2", cell_size / 2)
+                .attr("y2", cell_size / 2)
+                .attr("transform", "rotate(-15, " + cell_size / 2 + " " + cell_size / 2 + ")")
+                .style("stroke", speed_color)
+                .style("stroke-width", speed_bold)
+                .on("mouseover", function (d) {
+                    speed_mouseover(d, cell);
+                })
+                .on("mouseout", function () {
+                    cell.select(".rect_layer").remove();
+                    cell.select(".text_layer").remove();
+                })
+        })
+    })
 }
 
-function speed_color(d, i){
-	var length = Math.sqrt(d["speed_x"] * d["speed_x"] + d["speed_y"] * d["speed_y"]);
-	if(length > 0.5)  return "#6A52A4";
-	else if(length > 0.4) return "#4C9B71";
-	else if(length > 0.3) return "#64BD79"
-	else return "#9edae5";
+function speed_color(d, i) {
+    var length = Math.sqrt(d["speed_x"] * d["speed_x"] + d["speed_y"] * d["speed_y"]);
+    if (length > 0.5) return "#6A52A4";
+    else if (length > 0.4) return "#4C9B71";
+    else if (length > 0.3) return "#64BD79"
+    else return "#9edae5";
 }
 
-function speed_bold(d, i){
-	var length = Math.sqrt(d["speed_x"] * d["speed_x"] + d["speed_y"] * d["speed_y"]);
-	if(length > 1)  return "5";
-	else if(length > 0.15) return "4";
-	else return "3";
+function speed_bold(d, i) {
+    var length = Math.sqrt(d["speed_x"] * d["speed_x"] + d["speed_y"] * d["speed_y"]);
+    if (length > 1) return "5";
+    else if (length > 0.15) return "4";
+    else return "3";
 }
 
-function acc_bold(d, i){
-	var length = Math.sqrt(d["acc_x"] * d["acc_x"] + d["acc_y"] * d["acc_y"]);
-	if(length > 1)  return "5";
-	else if(length > 0.15) return "4";
-	else return "4";
+function acc_bold(d, i) {
+    var length = Math.sqrt(d["acc_x"] * d["acc_x"] + d["acc_y"] * d["acc_y"]);
+    if (length > 1) return "5";
+    else if (length > 0.15) return "4";
+    else return "4";
 }
 
-function acc_color(d, i){
-	var length = Math.sqrt(d["acc_x"] * d["acc_x"] + d["acc_y"] * d["acc_y"]);
-	if(length > 1)  return "#6A52A4";
-	else if(length > 0.15) return "#4C9B71";
-	else return "#64BD79";
+function acc_color(d, i) {
+    var length = Math.sqrt(d["acc_x"] * d["acc_x"] + d["acc_y"] * d["acc_y"]);
+    if (length > 1) return "#6A52A4";
+    else if (length > 0.15) return "#4C9B71";
+    else return "#64BD79";
 }
 
 
-function acc_mouseover(d, cell, cell_size){
+function acc_mouseover(d, cell, cell_size) {
     var rect_layer = cell.append("g").attr("class", "rect_layer");
     var text_layer = cell.append("g").attr("class", "text_layer");
     var text_length = text_layer.append("text")
-      .attr("x", cell_size / 2)
-      .attr("y", cell_size / 2 + 3)
-      .attr("dy", "1.1em")
-      .attr("class", "tooltip_text")
-      .style("fill", "white")
-      .attr("render-order", 99)
-      .text(parseFloat(Math.sqrt(d["acc_x"] * d["acc_x"] +  d["acc_y"] * d["acc_y"])).toFixed(2) + "m/s²");
+        .attr("x", cell_size / 2)
+        .attr("y", cell_size / 2 + 3)
+        .attr("dy", "1.1em")
+        .attr("class", "tooltip_text")
+        .style("fill", "white")
+        .attr("render-order", 99)
+        .text(parseFloat(Math.sqrt(d["acc_x"] * d["acc_x"] + d["acc_y"] * d["acc_y"])).toFixed(2) + "m/s²");
 
-      rect_layer.append("rect")
-      .attr("class", "tooltip_rect")
-      .attr("x", cell_size / 2)
-      .attr("y", cell_size / 2)
-      .attr("rx", 5)
-      .attr("ry", 5)
-      .attr("width", 65)
-      .attr("height", 25)
-      .style("fill", "black")
-      .style("fill-opacity", ".");
+    rect_layer.append("rect")
+        .attr("class", "tooltip_rect")
+        .attr("x", cell_size / 2)
+        .attr("y", cell_size / 2)
+        .attr("rx", 5)
+        .attr("ry", 5)
+        .attr("width", 65)
+        .attr("height", 25)
+        .style("fill", "black")
+        .style("fill-opacity", ".");
 }
 
 
-
-function speed_mouseover(d, cell, cell_size){
+function speed_mouseover(d, cell, cell_size) {
     var rect_layer = cell.append("g").attr("class", "rect_layer");
     var text_layer = cell.append("g").attr("class", "text_layer");
     var text_length = text_layer.append("text")
-      .attr("x", cell_size / 2)
-      .attr("y", cell_size / 2 + 3)
-      .attr("dy", "1.1em")
-      .attr("class", "tooltip_text")
-      .style("fill", "white")
-      .attr("render-order", 99)
-      .text(parseFloat(Math.sqrt(d["speed_x"] * d["speed_x"] +  d["speed_y"] * d["speed_y"])).toFixed(2) + "m/s");
+        .attr("x", cell_size / 2)
+        .attr("y", cell_size / 2 + 3)
+        .attr("dy", "1.1em")
+        .attr("class", "tooltip_text")
+        .style("fill", "white")
+        .attr("render-order", 99)
+        .text(parseFloat(Math.sqrt(d["speed_x"] * d["speed_x"] + d["speed_y"] * d["speed_y"])).toFixed(2) + "m/s");
 
-      rect_layer.append("rect")
-      .attr("class", "tooltip_rect")
-      .attr("x", cell_size / 2)
-      .attr("y", cell_size / 2)
-      .attr("rx", 5)
-      .attr("ry", 5)
-      .attr("width", 65)
-      .attr("height", 25)
-      .style("fill", "black")
-      .style("fill-opacity", ".7");
+    rect_layer.append("rect")
+        .attr("class", "tooltip_rect")
+        .attr("x", cell_size / 2)
+        .attr("y", cell_size / 2)
+        .attr("rx", 5)
+        .attr("ry", 5)
+        .attr("width", 65)
+        .attr("height", 25)
+        .style("fill", "black")
+        .style("fill-opacity", ".7");
 }
 
--- heatmap.js
+++ heatmap.js
@@ -1,19 +1,45 @@
-function showheatmap() {
-    var svg = d3.select("svg"),
-        margin = {top: 50, right: 20, bottom: 30, left: 40},
+function showheatmap(state, playerId, blockSize) {
+    d3.selectAll("svg > *").remove();
+    if (state === 1) {
+        $("#explain").text("on-ball");
+        my_points = offensive_points[playerId];
+    } else {
+        $("#explain").text("off-ball");
+        my_points = defensive_points[playerId];
+    }
+    var svg = d3.select("#main_panel").append("svg")
+        .attr("id", "east")
+        .attr("width", 600)
+        .attr("height", 400);
+
+    margin = {top: 50, right: 20, bottom: 30, left: 40},
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom,
         g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    g.append("line")
+        .attr("x1", -10)
+        .attr("y1", -10)
+        .attr("x2", width - margin.right)
+        .attr("y2", -10)
+        .style("stroke", "red")
+        .style("stroke-width", "6px");
+
     x_scales = width / 94.0;
     y_scales = height / 50.0;
-    console.log(my_points);
     var points = d3.range(my_points.length).map(function (i) {
         return [my_points[i][0] * x_scales, (50 - my_points[i][1]) * y_scales];
     });
-    var color = d3.scaleSequential(d3.interpolateLab("white", "steelblue"))
-        .domain([0, 200]);
+    var color = d3.interpolateLab("white", "steelblue");
+    var scale_linear = d3.scale.linear()
+        .domain([1, 5])
+        .range([200, 800]);
 
-    var radius = d3.scaleSqrt()
+    var color_linear = d3.scale.linear()
+        .domain([0, scale_linear(blockSize)])    //400 is proper
+        .range([0, 1]);
+
+    var radius = d3.scale.sqrt()
         .domain([0, 500])
         .range([0, 10]);
 
@@ -21,11 +47,11 @@ function showheatmap() {
         .radius(10)
         .extent([[0, 0], [94, 50]]);
 
-    var x = d3.scaleLinear()
+    var x = d3.scale.linear()
         .domain([0, 94])
         .range([0, width]);
 
-    var y = d3.scaleLinear()
+    var y = d3.scale.linear()
         .domain([0, 50])
         .range([height, 0]);
 
@@ -34,27 +60,29 @@ function showheatmap() {
         .append("rect")
         .attr("width", width)
         .attr("height", height);
-
+    // hexpts = hexbin(points);
     g.append("g")
         .attr("class", "hexagon")
         .attr("clip-path", "url(#clip)")
         .selectAll("path")
         .data(hexbin(points))
         .enter().append("path")
-        .attr("d", function (d) { return hexbin.hexagon(radius(Math.min(d.length, 500)));})
+        .attr("d", function (d) {
+            return hexbin.hexagon(radius(Math.min(d.length, 500)));
+        })
         .attr("transform", function (d) {
             return "translate(" + d.x + "," + d.y + ")";
         })
         .attr("fill", function (d) {
-            return color(d.length);
+            return color(color_linear(d.length));
         });
 
     g.append("g")
         .attr("class", "axis axis--y")
-        .call(d3.axisLeft(y).tickSizeOuter(-width));
+        .call(d3.svg.axis().scale(y).outerTickSize(1).orient("left"));
 
     g.append("g")
         .attr("class", "axis axis--x")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).tickSizeOuter(-height));
+        .call(d3.svg.axis().scale(x).outerTickSize(1).orient("bottom"));
 }
